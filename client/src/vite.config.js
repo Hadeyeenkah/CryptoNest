54818d577aa30f4bb41cc6fa196faec9714a2d2a
samuel Adeyinka <tboysammy@gmail.com>
2025-07-10 01:43:15 +0500

    cryptonest

--- client/src/vite.config.js
+++ client/src/vite.config.js
@@ -1,8 +1,12 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1500, // increases warning limit to 1.5MB
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:5173'
